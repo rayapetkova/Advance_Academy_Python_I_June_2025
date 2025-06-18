a95b281c6ce0dd245083dbe6e67038879b60412a
Raya Petkova <rayapetkova2007@abv.bg>
2025-06-18 16:57:51 +0300

    Lesson 1 and Lesson 2

---
+++ lesson_2/task_1.py
@@ -0,0 +1,4 @@
+animal_type = input("Кое ти е любимото животно?: ")
+animal_name = input("Как се казва то?: ")
+
+print(f"Любимото ми животно е {animal_type}. То се казва {animal_name}.")
---
+++ lesson_2/task_2.py
@@ -0,0 +1,4 @@
+apples = int(input("Колко ябълки имаш?: "))
+more_apples = apples + 3
+
+print(f"Вече имам {more_apples} ябълки.")
